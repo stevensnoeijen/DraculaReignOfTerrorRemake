de24fdbe4a809dd0ab646ac5202300a89e9a60cd
stevensnoeijen <stevensnoeijen@gmail.com>
2021-01-02 17:53:54 +0100

    implemented double click move action

--- src/systems/PlayerControlSystem.ts
+++ src/systems/PlayerControlSystem.ts
@@ -26,8 +26,8 @@ export class PlayerControlSystem extends System {
 
 	private inputEventsQueue: (KeyboardEvent | MouseEvent)[] = [];
 	private inputHandler: InputHandler;
-
-	private dragging = false;
+	private dragged = false;
+	private lastClickedTimeStamp = 0;
 
 	constructor(world: World, attributes: Attributes) {
 		super(world, attributes);
@@ -42,9 +42,6 @@ export class PlayerControlSystem extends System {
 		this.canvas.addEventListener('mousedown', this.handleMouseDown);
 		this.canvas.addEventListener('mousemove', this.handleMouseMove);
 		this.canvas.addEventListener('mouseup', this.handleMouseUp);
-		this.canvas.addEventListener('dblclick', () => {
-			console.log('double clicked');
-		});
 
 		// disable right button click inside canvas
 		this.canvas.addEventListener('contextmenu', (event) => {
@@ -62,29 +59,51 @@ export class PlayerControlSystem extends System {
 		this.processInputEvents();
 	}
 
-	private processInputEvents(): void {
-		if (this.inputEventsQueue.length > 0) {
-			let event;
-			while ((event = this.inputEventsQueue.pop())) {
-				if ('key' in event) {
-					this.inputHandler.handle(event);
-				} else {
-					// mouse event
-
-				}
-			}
-		}
-	}
-
 	public getSelector(): Entity {
 		return this.queries.selector.results[0];
 	}
 
+	private deselectUnits(): void {
+		console.log('deselectin');
+		this.getSelected().forEach(EntityHelper.deselect);
+	}
+
+	private selectUnits(): boolean {
+		const selector = this.getSelector();
+
+		const size = selector.getComponent(SizeComponent);
+		if (!size) {
+			return false;
+		}
+		if (0 === size.width && 0 === size.height) {
+			console.log('select 1');
+			const position = selector.getComponent(PositionComponent);
+			if (!position) {
+				return false;
+			}
+
+			// get entity at click location
+			this.selectEntityAtPosition(position.x, position.y);
+		} else {
+			console.log('select multiple');
+			// get entities inside selector
+			this.queries.selectable.results
+				.filter((entity) => EntityHelper.isObjectInsideContainer(entity, selector))
+				.forEach(EntityHelper.select);
+		}
+	}
+
+	private processInputEvents(): void {
+		if (this.inputEventsQueue.length > 0) {
+			let event;
+			while ((event = this.inputEventsQueue.pop())) {
+				this.inputHandler.handle(event);
+			}
+		}
+	}
+
 	private handleMouseDown(event: MouseEvent): void {
 		if (0 === event.button) {
-			this.getSelected().forEach(EntityHelper.deselect);
-			this.dragging = true;
-
 			const selector = this.getSelector();
 			const position = selector.getMutableComponent(PositionComponent);
 			if (undefined === position) {
@@ -111,7 +130,8 @@ export class PlayerControlSystem extends System {
 	}
 
 	private handleMouseMove(event: MouseEvent): void {
-		if (this.dragging) {
+		if (1 === event.buttons) {
+			this.dragged = true;
 			const selector = this.getSelector();
 			const position = selector.getComponent(PositionComponent);
 			if (!position) {
@@ -131,38 +151,60 @@ export class PlayerControlSystem extends System {
 	}
 
 	private handleMouseUp(event: MouseEvent): void {
+		console.log('up')
 		if (0 === event.button) {
-			this.dragging = false;
-
-			const selector = this.getSelector();
-			const visibility = selector.getMutableComponent(VisibilityComponent);
-			if (!visibility) {
-				return;
-			}
-
-			visibility.visible = false;
-
-			const size = selector.getComponent(SizeComponent);
-			if (!size) {
-				return;
-			}
-			if (0 === size.width && 0 === size.height) {
-				// get entity at click location
-				this.selectEntityAtPosition(event.offsetX, event.offsetY);
+			if (this.lastClickedTimeStamp !== 0 && Date.now() - this.lastClickedTimeStamp <= 250) {
+				console.log('double');
+				this.moveUnits(event.offsetX, event.offsetY);
 			} else {
-				// get entities inside selector
-				this.queries.selectable.results
-					.filter((entity) => EntityHelper.isObjectInsideContainer(entity, selector))
-					.forEach(EntityHelper.select);
+				// check if selecting a unit
+				const selector = this.getSelector();
+				const size = selector.getComponent(SizeComponent);
+				if (!size) {
+					return;
+				}
+				if (size.width === 0 || size.height === 0) {
+					const position = selector.getComponent(PositionComponent);
+					if (!position) {
+						return;
+					}
+
+					const entity = this.getEntityAtPosition(position.x, position.y);
+					if (null === entity) {
+						if (0 !== this.lastClickedTimeStamp && Date.now() - this.lastClickedTimeStamp > 250) {
+							this.lastClickedTimeStamp = Date.now();
+						} else {
+							this.deselectUnits();
+							this.lastClickedTimeStamp = 0;
+						}
+						return;// do nothing, this might be a move action
+					}
+				}
+
+				this.deselectUnits();
+				console.log('selected units');
+				this.selectUnits();
+				this.lastClickedTimeStamp = 0;
+
+				const visibility = selector.getMutableComponent(VisibilityComponent);
+				if (!visibility) {
+					return;
+				}
+
+				visibility.visible = false;
 			}
 		} else if (2 === event.button) {
 			// move unit
 			this.moveUnits(event.offsetX, event.offsetY);
 		}
+		this.lastClickedTimeStamp = Date.now();
+		this.dragged = false;
 	}
 
 	private moveUnits(x: number, y: number): void {
 		this.getSelected().forEach((entity) => {
+			console.log('move unit');
+
 			const position = entity.getComponent(PositionComponent);
 			if (!position) {
 				return;
@@ -186,13 +228,18 @@ export class PlayerControlSystem extends System {
 		this.inputEventsQueue.push(event);
 	}
 
-	private selectEntityAtPosition(x: number, y: number): void {
+	/**
+	 * @returns {boolean} if a entity is selected
+	 */
+	private selectEntityAtPosition(x: number, y: number): boolean {
 		const entity = this.getEntityAtPosition(x, y);
 		if (null === entity) {
-			return;
+			return false;
 		}
 
 		EntityHelper.select(entity);
+
+		return true;
 	}
 
 	private getEntityAtPosition(x: number, y: number): Entity | null {
