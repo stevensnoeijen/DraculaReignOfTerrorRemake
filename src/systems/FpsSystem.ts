9223938146e61ce83a6ee04320e07a0bdfeaf9f3
stevensnoeijen <stevensnoeijen@gmail.com>
2021-01-02 14:11:23 +0100

    fix FpsSystem

--- src/systems/FpsSystem.ts
+++ src/systems/FpsSystem.ts
@@ -9,12 +9,12 @@ export class FpsSystem extends System {
 			components: [FpsComponent],
 		},
 	};
-	private static readonly UPDATE_TIME = 10000;
+	private static readonly UPDATE_TIME = 1000;
 
 	private update = 1000;
 
 	public execute(delta: number, time: number): void {
-		this.update += time;
+		this.update += delta;
 		if (this.update > FpsSystem.UPDATE_TIME) {
 			// Iterate through all the entities on the query
 			this.queries.fps.results.forEach((entity) => {
