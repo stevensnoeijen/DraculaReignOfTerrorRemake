2a57734b08b7b90d27d4865b4202b8882f84de50
stevensnoeijen <stevensnoeijen@gmail.com>
2021-02-22 21:27:15 +0100

    add jdoc to Velocity components

--- src/components/MoveTransformVelocityComponent.ts
+++ src/components/MoveTransformVelocityComponent.ts
@@ -2,16 +2,20 @@ import { Component, Types } from 'ecsy';
 import { Vector2 } from '../math/Vector2';
 
 export interface IMoveTransformVelocityComponentProps {
-    moveSpeed?: number;
-    velocity?: Vector2;
+	moveSpeed?: number;
+	velocity?: Vector2;
 }
 
+/**
+ * Moves entity with velocity.
+ * Use {@link MoveVelocityComponent} when physics is required.
+ */
 export class MoveTransformVelocityComponent extends Component<IMoveTransformVelocityComponentProps> {
-    moveSpeed!: number;
-    velocity!: Vector2;
+	moveSpeed!: number;
+	velocity!: Vector2;
 }
 
 MoveTransformVelocityComponent.schema = {
-    moveSpeed: { type: Types.Number, default: 100 },
-    velocity: { type: Types.Ref, default: Vector2.ZERO },
+	moveSpeed: { type: Types.Number, default: 100 },
+	velocity: { type: Types.Ref, default: Vector2.ZERO },
 };
