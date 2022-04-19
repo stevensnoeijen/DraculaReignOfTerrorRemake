1ffc4165b163815b42f7e1778715658e91a4a1d0
Steven Snoeijen <stevensnoeijen@gmail.com>
2022-04-19 21:18:09 +0200

    test: fix toGridPosition

--- src/systems/player/utils.spec.ts
+++ src/systems/player/utils.spec.ts
@@ -1,11 +1,10 @@
-import * as PIXI from 'pixi.js';
 import { Vector2 } from '../../math/Vector2';
 
 import { toGridPosition } from './utils';
 
 describe('toGridPosition', () => {
     it('should center to grid position', () => {
-        const position = toGridPosition({ screen: { width: 1000, height: 1000 } } as PIXI.Application, new Vector2(101, 60));
+        const position = toGridPosition(new Vector2(101, 60));
 
         expect(position.x).toEqual(104);
         expect(position.y).toEqual(56);
