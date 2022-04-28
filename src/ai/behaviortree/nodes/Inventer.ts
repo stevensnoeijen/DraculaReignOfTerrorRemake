c644878ac17c2282e3df1261ef99f86bea58ca86
Steven Snoeijen <stevensnoeijen@gmail.com>
2022-04-28 15:12:10 +0200

    feat: Inventer

---
+++ src/ai/behaviortree/nodes/Inventer.ts
@@ -0,0 +1,25 @@
+import { Node, State } from "./Node";
+
+const stateMap = new Map<State, State>([
+    [State.FAILURE, State.SUCCESS],
+    [State.SUCCESS, State.FAILURE],
+    [State.RUNNING, State.RUNNING],
+]);
+
+/**
+ * This is the "Not" operator (!).
+ */
+export class Inventer extends Node {
+    public evaluate(): State {
+        if (!this.hasChildren()) {
+            return State.FAILURE;
+        }
+        
+        const childState = this.children[0].evaluate();
+        if (!stateMap.has(childState)) {
+            return State.FAILURE;
+        }
+
+        return this.state = stateMap.get(childState)!;
+    }
+}
