745a42af67d18eac97b7bd6073934fd34d88639b
Steven Snoeijen <stevensnoeijen@gmail.com>
2022-04-28 14:58:40 +0200

    feat: Sequence

---
+++ src/ai/behaviortree/nodes/Sequence.ts
@@ -0,0 +1,25 @@
+import { Node, State } from "./Node";
+
+/**
+ * This is the "And" operator.
+ */
+export class Sequence extends Node {
+    public evaluate(): State {
+        let anyChildIsRunning = false;
+
+        for(const child of this.children) {
+            switch(child.evaluate()) {
+                case State.RUNNING:
+                    anyChildIsRunning = true;
+                    continue;
+                case State.SUCCESS:
+                    continue;
+                case State.FAILURE:
+                default:
+                    return this.state = State.FAILURE;
+            }
+        }
+
+        return this.state = anyChildIsRunning ? State.RUNNING : State.SUCCESS;
+    }
+}
