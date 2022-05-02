c5972daef9df7f1dfd0fab1ddd87dc2867349ad0
Steven Snoeijen <stevensnoeijen@gmail.com>
2022-05-02 21:58:57 +0200

    feat: add Follow for behavoirtree

---
+++ src/ai/behaviortree/nodes/entity/Follow.ts
@@ -0,0 +1,21 @@
+import { TargetComponent } from './../../../../systems/TargetComponent';
+import { Entity } from "ecsy";
+
+import { State } from "../Node";
+import { EntityNode } from "./EntityNode";
+import { FollowComponent } from './../../../../systems/movement/FollowComponent';
+
+export class Follow extends EntityNode {
+    protected evaluateByEntity(entity: Entity): State {
+        const targetComponent = entity.getComponent(TargetComponent);
+        const followComponent = entity.getMutableComponent(FollowComponent);
+
+        if (targetComponent == null || followComponent == null) {
+            return this.failure();
+        }
+
+        followComponent.follow = targetComponent.target;
+
+        return this.success();
+    }
+}
