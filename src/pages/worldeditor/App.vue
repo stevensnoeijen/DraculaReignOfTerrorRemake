1ec823e019d5c8fa6478c7fe80f8dce7860cbfaf
Steven Snoeijen <stevensnoeijen@gmail.com>
2022-07-13 22:12:26 +0200

    feat: add page for worldeditor

---
+++ src/pages/worldeditor/App.vue
@@ -0,0 +1,11 @@
+
+<template>
+	<div>
+		World Editor
+	</div>
+</template>
+
+<script setup lang="ts">
+const text = '1';
+
+</script>
---
+++ src/pages/worldeditor/main.ts
@@ -0,0 +1,13 @@
+import { createApp } from 'vue';
+;
+import App from './App.vue';
+
+declare module '@vue/runtime-core' {
+    export interface ComponentCustomProperties {
+        window: typeof Window,
+    }
+}
+
+const app = createApp(App);
+app.config.globalProperties.window = window;
+app.mount('#app');
