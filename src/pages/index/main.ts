45c9dbb8bb601a11b291cf113de2ec34acf8f12c
Steven Snoeijen <stevensnoeijen@gmail.com>
2022-07-13 22:17:32 +0200

    refactor: move global typeoverrides to src/types dir

--- src/pages/index/main.ts
+++ src/pages/index/main.ts
@@ -1,12 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-declare module '@vue/runtime-core' {
-    export interface ComponentCustomProperties {
-        window: typeof Window,
-    }
-}
-
 const app = createApp(App);
 app.config.globalProperties.window = window;
 app.mount('#app');
--- src/pages/worldeditor/main.ts
+++ src/pages/worldeditor/main.ts
@@ -2,12 +2,6 @@ import { createApp } from 'vue';
 ;
 import App from './App.vue';
 
-declare module '@vue/runtime-core' {
-    export interface ComponentCustomProperties {
-        window: typeof Window,
-    }
-}
-
 const app = createApp(App);
 app.config.globalProperties.window = window;
 app.mount('#app');
