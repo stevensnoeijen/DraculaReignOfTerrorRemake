cad9db609089a8759308805dcfb4b100fbb94971
Steven Snoeijen <stevensnoeijen@gmail.com>
2022-07-16 20:32:46 +0200

    fix: type errors in UnitEditor.vue

--- src/components/worldeditor/UnitEditor.vue
+++ src/components/worldeditor/UnitEditor.vue
@@ -2,32 +2,34 @@
   <div>
     <h1 class="text-xl font-bold">Unit editor</h1>
     <div>
-      <pixi-application ref="pixi" :width="500" :height="500" />
+      <pixi-application ref="pixi" :width="500" :height="500"/>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { $ref } from "vue/macros";
+import { ref } from "vue";
 import * as PIXI from 'pixi.js';
 
 import { PixiApplicationInstance } from "../../components/pixi/types";
 import * as animations from '../../game/animations';
 import { onMounted } from "vue";
 
-const pixi = $ref<PixiApplicationInstance>();
+const pixi = ref<PixiApplicationInstance>();
 
 onMounted(() => {
-  pixi.application.loader.add('unit', '/assets/unit.json')
+  const app = pixi.value!.application;
+
+  app.loader.add('unit', '/assets/unit.json')
     .load(() => {
-      const unitAnimations = animations.load(pixi.application.loader.resources.unit.spritesheet!);
+      const unitAnimations = animations.load(app.loader.resources.unit.spritesheet!);
       const unitAnimation = unitAnimations['red'].swordsmen;
       const sprite = new PIXI.AnimatedSprite(unitAnimation.move.north);
       sprite.anchor.set(0.5);
-      sprite.position.set(pixi.application.view.width / 2, pixi.application.view.height / 2);
+      sprite.position.set(app.view.width / 2, app.view.height / 2);
       sprite.animationSpeed = .25;
 		  sprite.play();
-      pixi.application.stage.addChild(sprite);
+      app.stage.addChild(sprite);
     });
 });
 </script>
