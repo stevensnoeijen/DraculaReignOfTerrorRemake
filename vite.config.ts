98e52ad4e6db51d56669fd4aeaacc5a2963b606e
Steven Snoeijen <stevensnoeijen@gmail.com>
2022-07-24 15:21:18 +0200

    chore: remove vite-plugin-vue-type-imports as it is not working atm

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import Vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
-import VueTypeImports from 'vite-plugin-vue-type-imports';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +11,5 @@ export default defineConfig({
     Components({
       dts: true,
     }),
-    VueTypeImports(),
   ],
 });
