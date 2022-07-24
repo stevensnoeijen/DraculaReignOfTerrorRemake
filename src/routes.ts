9b2a384b37d9cc206e292ffbc5ee0e9337b3010c
Steven Snoeijen <stevensnoeijen@gmail.com>
2022-07-24 17:05:09 +0200

    tryout commit with lint warning added

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,9 @@
 import { RouteRecordRaw } from 'vue-router';
+
 import GameView from './views/GameView.vue';
 import WorldEditorView from './views/WorldEditorView.vue';
 
 export const routes: readonly RouteRecordRaw[] = [
   { path: '/', redirect: '/game' },
-  { path: '/game', component: GameView },
-  { path: '/worldeditor', component: WorldEditorView },
+  { path: '/game', component: GameView }, { path: '/worldeditor', component: WorldEditorView },
 ];
