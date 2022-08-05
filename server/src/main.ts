bd6abcc7c94401850c0179bd44f327e666ddb476
Steven Snoeijen <stevensnoeijen@gmail.com>
2022-08-05 15:58:06 +0200

    feat: enable cors on server

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,8 +2,10 @@ import fs from 'fs';
 
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 const port = 3000;
 
