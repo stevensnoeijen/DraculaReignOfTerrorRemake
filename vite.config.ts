4c57782b928b8d6c658f8f758f543edfa4c0676f
Steven Snoeijen <stevensnoeijen@gmail.com>
2022-08-09 12:42:29 +0200

    fix: tspath in web feat: enable decorators in jest tests

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import Vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
 import Checker from 'vite-plugin-checker';
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
@@ -19,5 +20,6 @@ export default defineConfig({
       dts: true,
       resolvers: [NaiveUiResolver()],
     }),
+    tsconfigPaths(),
   ],
 });
