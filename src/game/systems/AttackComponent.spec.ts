496c1de3e34a78bfd745d3bd4efb374bfb3df011
Steven Snoeijen <stevensnoeijen@gmail.com>
2022-08-29 21:48:39 +0200

    refactor: fix test

--- src/game/systems/AttackComponent.spec.ts
+++ src/game/systems/AttackComponent.spec.ts
@@ -1,15 +1,25 @@
+import { buildWorld } from 'sim-ecs';
 import { World } from 'ecsy';
 
+import { Health } from '../components/Health';
+
 import { AttackComponent } from './AttackComponent';
-import { HealthComponent } from './health/HealthComponent';
+import { SimEcsComponent } from './SimEcsComponent';
+
 
 describe('AttackComponent', () => {
   describe('attack', () => {
-    const world = new World().registerComponent(HealthComponent);
+    const newWorld = buildWorld().build();
+    const world = new World().registerComponent(SimEcsComponent);
 
     it('should take hit to enemy', () => {
-      const enemy = world.createEntity().addComponent(HealthComponent, {
-        points: 100,
+      const enemy = world.createEntity().addComponent(SimEcsComponent, {
+        entity: newWorld.buildEntity()
+          .with(new Health({
+            points: 100,
+            maxPoints: 100,
+          }))
+        .build(),
       });
       const attackComponent = new AttackComponent({
         attackDamage: 10,
@@ -17,7 +27,9 @@ describe('AttackComponent', () => {
 
       attackComponent.attack(enemy);
 
-      expect(enemy.getComponent(HealthComponent)!.points).toEqual(90);
+      expect(enemy.getComponent(SimEcsComponent)!
+        .entity.getComponent(Health)!
+        .points).toEqual(90);
     });
   });
 });
