749a6fa5e4cb4cd5580402aa52bf54bbb85a8194
Steven Snoeijen <stevensnoeijen@gmail.com>
2022-08-31 21:50:02 +0200

    refactor: abstract selection logic to MouseSelection

--- src/game/systems/player/PlayerSelectionSystem.ts
+++ src/game/systems/player/PlayerSelectionSystem.ts
@@ -3,22 +3,15 @@ import * as PIXI from 'pixi.js';
 import { createSystem, queryComponents, Read, ReadResource, ReadEntity } from 'sim-ecs';
 
 import { EntityHelper } from '../../EntityHelper';
-import { Vector2 } from '../../math/Vector2';
 import { Input } from '../../Input';
 import { getEntityAtPosition, getSimComponent } from '../utils';
 import { Selectable } from '../../components/player/Selectable';
 import { isOnTeam } from '../utils/index';
 
 import { EcsyEntity } from './../../components/EcsyEntity';
+import { MouseSelection } from './MouseSelection';
 
-const SINGLE_UNIT_DISTANCE = 5;
-
-/**
- * Start selection.
- */
-let startPosition: Vector2 | null = null;
-const rectangle = new PIXI.Graphics();
-rectangle.visible = false;
+const mouseSelection = new MouseSelection();
 
  /**
    * Used for deselecting units when clicking,
@@ -33,52 +26,33 @@ const getSelected = (entities: Entity[]): Entity[] => {
 };
 
 const selectEntities = (entities: Entity[]) => {
-  if (isSimpleClick()) {
+  if (mouseSelection.isSimpleClick()) {
     selectOneEntity(entities);
   } else {
     selectEntitiesInsideSelectionRectangle(entities);
   }
 };
 
-const startSelect = () => {
-  rectangle.visible = true;
-};
-
 const updateSelect = (entities: Entity[]) => {
-  const width = Input.mousePosition.x - startPosition!.x;
-  const height = Input.mousePosition.y - startPosition!.y;
-
+  mouseSelection.update();
   getSelected(entities)
     .forEach(EntityHelper.deselect);
-
-  rectangle.clear();
-  rectangle.lineStyle(1, 0x000000);
-  rectangle.drawRect(
-    startPosition!.x,
-    startPosition!.y,
-    width,
-    height
-  );
 };
 
 const endSelect = (entities: Entity[]) => {
   selectEntities(entities);
-
-  startPosition = null;
-
-  rectangle.visible = false;
-  rectangle.clear();
+  mouseSelection.end();
 };
 
 const selectEntitiesInsideSelectionRectangle = (entities: Entity[]) => {
-  const width = Input.mousePosition.x - startPosition!.x;
-  const height = Input.mousePosition.y - startPosition!.y;
+  const width = Input.mousePosition.x - mouseSelection.startPosition!.x;
+  const height = Input.mousePosition.y - mouseSelection.startPosition!.y;
 
   // get entities inside selector
   entities.filter((entity) =>
       EntityHelper.isObjectInsideContainer(entity, {
-        x: startPosition!.x,
-        y: startPosition!.y,
+        x: mouseSelection.startPosition!.x,
+        y: mouseSelection.startPosition!.y,
         width,
         height,
       })
@@ -86,14 +60,6 @@ const selectEntitiesInsideSelectionRectangle = (entities: Entity[]) => {
     .filter(isOnTeam(1))
     .forEach((entity) => EntityHelper.select(entity));
 };
-
-const isSimpleClick = (): boolean => {
-  return (
-    Vector2.distance(startPosition!, Input.mousePosition) <
-    SINGLE_UNIT_DISTANCE
-  );
-};
-
 const selectOneEntity = (entities: Entity[]) => {
   // single unit select
   const entity = getEntityAtPosition(
@@ -115,6 +81,24 @@ const selectOneEntity = (entities: Entity[]) => {
   }
 };
 
+const updateMouseSelection = (entities: Entity[]) => {
+  if (Input.isMouseDblClick() && deselectEntitiesTimeout !== null) {
+    clearTimeout(deselectEntitiesTimeout);
+  }
+
+  if (mouseSelection.started) {
+    if (Input.isMouseButtonUp(0)) {
+      endSelect(entities);
+    } else if (mouseSelection.isDragging()) {
+      updateSelect(entities);
+    }
+  }
+
+  if (!mouseSelection.started && Input.isMouseButtonDown(0)) {
+    mouseSelection.start();
+  }
+};
+
 /**
  * System for selecting units with {@link Input}'s' mouse.
  */
@@ -126,37 +110,15 @@ export const PlayerSelectionSystem = createSystem({
   }),
 })
 .withSetupFunction(({ app }) => {
-  app.stage.addChild(rectangle);
+
+  app.stage.addChild(mouseSelection.rectangle);
 })
 .withRunFunction(({
   query
 }) => {
-  const entities = Array.from(query.iter()).map(e => e.entity.getComponent(EcsyEntity)!.entity);
+  const entities = Array.from(query.iter())
+    .map(e => e.entity.getComponent(EcsyEntity)!.entity);
 
-  if (Input.isMouseDblClick() && deselectEntitiesTimeout !== null) {
-    clearTimeout(deselectEntitiesTimeout);
-  }
-
-  if (startPosition === null) {
-    if (Input.isMouseButtonDown(0)) {
-      startPosition = Input.mousePosition;
-      startSelect();
-    }
-  } else {
-    // selector is active
-    if (Input.isMouseButtonUp(0)) {
-      endSelect(entities);
-    } else if (
-      Vector2.distance(startPosition, Input.mousePosition) >
-      SINGLE_UNIT_DISTANCE
-    ) {
-      // mouse is dragging
-      updateSelect(entities);
-    }
-  }
-
-  query.execute(({ selectable }) => {
-
-  });
+  updateMouseSelection(entities);
 })
 .build();
