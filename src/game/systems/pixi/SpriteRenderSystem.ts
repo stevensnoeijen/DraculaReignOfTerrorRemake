0d7e9d7ca723a14b7e87de46e109ba5853cadb6b
Steven Snoeijen <stevensnoeijen@gmail.com>
2022-09-06 20:31:00 +0200

    feat: move sprite to dead-layer when died

--- src/game/systems/pixi/SpriteRenderSystem.ts
+++ src/game/systems/pixi/SpriteRenderSystem.ts
@@ -4,8 +4,10 @@ import * as PIXI from 'pixi.js';
 import { Transform } from '../../components/Transform';
 
 import { Alive } from '~/game/components/Alive';
+import { Died } from '~/game/events/Died';
 
 let aliveLayer: PIXI.Container;
+let deadLayer: PIXI.Container;
 
 const updatePosition = (sprite: PIXI.AnimatedSprite | PIXI.Sprite, transform: Transform) => {
   sprite.position.set(
@@ -17,18 +19,21 @@ const updatePosition = (sprite: PIXI.AnimatedSprite | PIXI.Sprite, transform: Tr
 export const SpriteRenderSystem = createSystem({
   app: WriteResource(PIXI.Application),
   entityAdded: ReadEvents(EntityAdded),
+  died: ReadEvents(Died),
+
   query: queryComponents({
     sprite: ReadOptional(PIXI.Sprite),
     animatedSprite: ReadOptional(PIXI.AnimatedSprite),
     transform: Read(Transform),
-    alive: Read(Alive),
   }),
 })
 .withSetupFunction(({ app }) => {
+  deadLayer = app.stage.addChildAt(new PIXI.Container(), 0);
   aliveLayer = app.stage.addChildAt(new PIXI.Container(), 1);
 })
 .withRunFunction(({
   entityAdded,
+  died,
   query,
 }) => {
   entityAdded.execute(event => {
@@ -39,13 +44,25 @@ export const SpriteRenderSystem = createSystem({
       aliveLayer.addChild(event.entity.getComponent(PIXI.AnimatedSprite)!);
   });
 
-  query.execute(({ sprite, animatedSprite, transform, alive }) => {
+  died.execute(event => {
+    if (event.entity.hasComponent(Alive)) {
+      let sprite: PIXI.Sprite | null = null;
+      if (event.entity.hasComponent(PIXI.Sprite))
+        sprite = event.entity.getComponent(PIXI.Sprite)!;
+      if (event.entity.hasComponent(PIXI.AnimatedSprite))
+        sprite = event.entity.getComponent(PIXI.AnimatedSprite)!;
+
+      if (sprite == null) return;
+
+      aliveLayer.removeChild(sprite!);
+      deadLayer.addChild(sprite!);
+    }
+  });
+
+  query.execute(({ sprite, animatedSprite, transform }) => {
     if (sprite != null) updatePosition(sprite as PIXI.Sprite, transform);
-    if (animatedSprite != null)
-      updatePosition(
-        animatedSprite as PIXI.AnimatedSprite,
-        transform,
-      );
+
+    if (animatedSprite != null) updatePosition(animatedSprite as PIXI.AnimatedSprite, transform);
   });
 })
 .build();
