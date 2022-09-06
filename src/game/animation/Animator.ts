4017fbd4e0ced41adba177f4a4f830e022be253e
Steven Snoeijen <stevensnoeijen@gmail.com>
2022-09-06 11:29:03 +0200

    fix: Animator keep state so only to update the sprite's animation when state is changed

--- src/game/animation/Animator.ts
+++ src/game/animation/Animator.ts
@@ -4,13 +4,25 @@ import { MoveDirection, UnitState } from '../types';
 
 import { AnimationModel } from './AnimationModel';
 export class Animator {
+  private currentState: UnitState= 'idle';
+  private currentDirection: MoveDirection = 'north';
+
   constructor(
     public readonly sprite: AnimatedSprite,
     public readonly model: AnimationModel
   ) {}
 
   public set(state: UnitState, direction: MoveDirection): void {
-    const animation = this.model.getAnimation(state, direction);
-    animation.set(this.sprite);
+    if (this.isStateChanged(state, direction)) {
+      const animation = this.model.getAnimation(state, direction);
+      animation.set(this.sprite);
+
+      this.currentState = state;
+      this.currentDirection = direction;
+    }
+  }
+
+  private isStateChanged (state: UnitState, direction: MoveDirection): boolean {
+    return this.currentState !== state || this.currentDirection != direction;
   }
 }
