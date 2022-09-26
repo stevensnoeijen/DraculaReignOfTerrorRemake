40214e525617d9c76636832784d2ed043d173ebd
Steven Snoeijen <stevensnoeijen@gmail.com>
2022-09-26 20:55:11 +0200

    fix: "Add Object" adding Range to default values

--- src/components/worldeditor/objects/create/utils.ts
+++ src/components/worldeditor/objects/create/utils.ts
@@ -6,17 +6,13 @@ import {
   getEditableProperties,
 } from '~/game/data/decorator';
 import { Unit } from '~/game/data/Unit';
+import { Range } from '~/game/utils/Range';
 
 export const getDefaultValueByType = <T extends unknown>(type: T): T => {
-  if (type === String) {
-    return '' as T;
-  }
-  if (type === Number) {
-    return 0 as T;
-  }
-  if (type === Boolean) {
-    return false as T;
-  }
+  if (type === String) return '' as T;
+  if (type === Number) return 0 as T;
+  if (type === Boolean) return false as T;
+  if (type === Range) return new Range(0, 0) as T;
 
   throw new Error(`Can not create default value of unknown type ${type}`);
 };
